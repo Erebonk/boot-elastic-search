56e5d862d1e194476e0aea3d5754103b4164357a
ilya <ilya.selfrealization@gmail.com>
2020-01-07 16:29:42 +0300

    refactoring

--- src/main/java/com/erebonk/elasticsearch/controller/ProductController.java
+++ src/main/java/com/erebonk/elasticsearch/controller/ProductController.java
@@ -3,12 +3,16 @@ package com.erebonk.elasticsearch.controller;
 import com.erebonk.elasticsearch.domain.Product;
 import com.erebonk.elasticsearch.service.product.ProductRepositoryService;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,17 +28,26 @@ public class ProductController {
 
     private final ProductRepositoryService productRepositoryService;
 
-//    @GetMapping
-//    public List<Product> findAllByName(@NotNull String name) {
-//        return productRepositoryService.findAllByName(name);
-//    }
-
     @GetMapping
-    public List<Product> findAll() {
-        List<Product> actualList = new ArrayList<>();
-        var productsIterator = productRepositoryService.findAll();
-        productsIterator.forEach(actualList::add);
-        return actualList;
+    public Iterable<Product> findAll() {
+        return productRepositoryService.findAll();
+    }
+
+    @GetMapping("/product")
+    public List<Product> findAllByName(@NotNull @RequestParam String name) {
+        return productRepositoryService.findAllByName(name);
+    }
+
+    @GetMapping("/search")
+    public Page<Product> searchByText(@NotNull @RequestParam String text) {
+        var sq = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery(text)
+                .field("name")
+                .field("rusName")
+                .field("vendor")
+                .type(MultiMatchQueryBuilder.Type.BEST_FIELDS))
+                .build();
+        return productRepositoryService.search(sq);
     }
 
 }
